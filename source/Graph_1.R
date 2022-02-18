e709d4b6cc9998d957919714cb1733aa65f0cf8f
Jiali Liu <Jiali123@uw.edu>
2022-02-18 12:23:24 -0800

    graph1

--- source/Graph_1.R
+++ source/Graph_1.R
@@ -3,7 +3,9 @@ library("ggplot2")
 
 wages <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-Trangtran62/main/data/wages.csv")
 
+options(scipen=999)
 graph1 <- ggplot(
   data = wages,
-  mapping = aes(x = race, y = earn, fill = race, options(scipen = 999))
+  mapping = aes(x = race, y = earn, fill = race)
 ) + geom_boxplot() + coord_flip() + ggtitle("Distribution of wage earned by race") + ylab("Amount of wages earned") + xlab("Race")
+graph1
