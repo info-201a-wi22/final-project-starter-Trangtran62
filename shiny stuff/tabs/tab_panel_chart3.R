48eacdc68b5d814549831412dc02f5524350d5f2
Yuguang Zhang <yzhangss@uw.edu>
2022-03-09 09:33:45 -0800

    Updating chart3

--- shiny stuff/tabs/tab_panel_chart3.R
+++ shiny stuff/tabs/tab_panel_chart3.R
@@ -2,7 +2,39 @@
 
 library(shiny)
 
+input_bar_one <- selectInput(
+  "bar_1",
+  label = "First Bar",
+  choices = list("White" = "white", "Black" = "black",
+                 "Hipanic" = "hispanic", "Other" = "other"),
+  selected = "white"
+)
+
+input_bar_two <- selectInput(
+  "bar_2",
+  label = "Second Bar",
+  choices = list("White" = "white", "Black" = "black",
+                 "Hipanic" = "hispanic", "Other" = "other"),
+  selected = "black"
+)
+
 tab_panel_chart3 <-tabPanel(
     "Chart 3",
-    p("This is chart 3.")
+    h1("Median Earning Between Different Racial Groups"),
+    
+    input_bar_one,
+    
+    input_bar_two,
+    
+    plotlyOutput("barplot"),
+    
+    p("This graph displays the disparity in earnings in relation with the 
+      intersection between race and gender. There is a clear wage gap 
+      determined by gender, with males earning higher income than females 
+      across all racial groups. According to the graph above (race and income), 
+      disparities in income are featured between different racial groups, with 
+      white people earning a disproportionately higher income than minoritized 
+      groups. However, when looking at the effect of gender and race together 
+      on income, the additional contribution of race to the wage gap is 
+      unclear, as white female is the group that has the least median income. ")
 )
